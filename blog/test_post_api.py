f2b404328a7a32794d394f9621e76a630384d787
Rahim Thorm <sourabrahim@gmail.com>
2025-02-22 09:41:59 +0000

    Finish testing DRF with requests

--- blog/test_post_api.py
+++ blog/test_post_api.py
@@ -10,32 +10,32 @@ from blog.models import Post
 
 class PostApiTestCase(TestCase):
   
-  def setUp(self):
-    self.u1 = get_user_model().objects.get_or_create(email="user1@gmail.com", password="pwd_user_1")
-    self.u2 = get_user_model().objects.get_or_create(email="user2@gmail.com", password="pwd_user_2")
-    self.posts = [
-      Post.objects.create(
-                author=self.u1,
-                published_at=timezone.now(),
-                title="Post 1 Title",
-                slug="post-1-slug",
-                content="Post 1 Content",
-      ),
-      Post.objects.create(
-                author=self.u2,
-                published_at=timezone.now(),
-                title="Post 2 Title",
-                slug="post-2-slug",
-                content="Post 2 Content",
-      ),
-    ]
-    # look up the post info by ID
-    self.post_lookup = {f"{p.id}": p for p in posts}
+    def setUp(self):
+      self.u1 = get_user_model().objects.get_or_create(email="user1@gmail.com", password="pwd_user_1")[0]
+      self.u2 = get_user_model().objects.get_or_create(email="user2@gmail.com", password="pwd_user_2")[0]
+      posts = [
+        Post.objects.create(
+                  author=self.u1,
+                  published_at=timezone.now(),
+                  title="Post 1 Title",
+                  slug="post-1-slug",
+                  content="Post 1 Content",
+        ),
+        Post.objects.create(
+                  author=self.u2,
+                  published_at=timezone.now(),
+                  title="Post 2 Title",
+                  slug="post-2-slug",
+                  content="Post 2 Content",
+        ),
+      ]
+      # look up the post info by ID
+      self.post_lookup = {p.id: p for p in posts}
 
-    # Override test client
-    self.client = APIClient()
-    token_u1 = Token.objects.create(user=self.u1)
-    self.credentials(HTTP_AUTHORIZATION="Token " + token_u1)
+      # Override test client
+      self.client = APIClient()
+      token_u1 = Token.objects.create(user=self.u1)
+      self.client.credentials(HTTP_AUTHORIZATION="Token " + str(token_u1))
 
 
 
@@ -50,7 +50,6 @@ class PostApiTestCase(TestCase):
           post_obj = self.post_lookup[post_dict["id"]]
           self.assertEqual(post_obj.title, post_dict["title"])
           self.assertEqual(post_obj.slug, post_dict["slug"])
-          self.assertEqual(post_obj.summary, post_dict["summary"])
           self.assertEqual(post_obj.content, post_dict["content"])
           self.assertTrue(
               post_dict["author"].endswith(f"/api/v1/users/{post_obj.author.email}")
@@ -70,9 +69,8 @@ class PostApiTestCase(TestCase):
       post_dict = {
           "title": "Test Post",
           "slug": "test-post-3",
-          "summary": "Test Summary",
           "content": "Test Content",
-          "author": "http://testserver/api/v1/users/test@example.com",
+          "author": "http://testserver/api/v1/users/user1@gmail.com",
           "published_at": "2021-01-10T09:00:00Z",
       }
       resp = self.client.post("/api/v1/posts/", post_dict)
@@ -81,21 +79,19 @@ class PostApiTestCase(TestCase):
 
     def test_post_create(self):
       """It should create a new post for authenticated user"""
-
       post_dict = {
           "title": "Test Post",
           "slug": "test-post-3",
-          "summary": "Test Summary",
           "content": "Test Content",
-          "author": "http://testserver/api/v1/users/test@example.com",
+          "author": "http://testserver/api/v1/users/user1@gmail.com",
           "published_at": "2021-01-10T09:00:00Z",
       }
       resp = self.client.post("/api/v1/posts/", post_dict)
+  
       post_id = resp.json()["id"]
       post = Post.objects.get(pk=post_id)
       self.assertEqual(post.title, post_dict["title"])
       self.assertEqual(post.slug, post_dict["slug"])
-      self.assertEqual(post.summary, post_dict["summary"])
       self.assertEqual(post.content, post_dict["content"])
       self.assertEqual(post.author, self.u1)
       self.assertEqual(post.published_at, datetime(2021, 1, 10, 9, 0, 0, tzinfo=UTC))
