b4d6d660d61802f8c851e4f381cb58da095bc488
Rahim Thorm <sourabrahim@gmail.com>
2025-02-24 23:19:55 +0000

    Finish simplejwt

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -6,6 +6,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -28,6 +29,8 @@ urlpatterns = [
 urlpatterns +=[
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
     path('swagger.<str:doc_format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0),name="schema-swagger-ui"),
     path("", include(router.urls)),
